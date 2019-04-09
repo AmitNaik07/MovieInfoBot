0b36406f0d1b1b62a247d989924d634f081f1e6f
Amit Naik <mailamit70@gmail.com>
2019-04-09 17:22:28 -0400

    Added functionality for input from user

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
 import requests
 
-def convert_uppercase(bot, update):
+def get_info(bot, update):
     text =update.message.text
     url ='http://www.omdbapi.com/?t='+text+'&apikey=9b24e033'
     contents = requests.get(url).json()
-    title = contents
-    print(title)
+    title = contents['Title']
+    year =contents['Year']
+    genre =contents['Genre']
+    awards =contents['Awards']
+    poster =contents['Poster']
+    ratings_Rotten =contents['Ratings'][1]['Value']
+    ratings_IMDB =contents['imdbRating']
+    output =title +"\n" + "Released in the year " +year +"\n" + "Awards: " + awards +"\n" + "Genre: "+genre +"\n" + "Rotten Tomatoes: " +ratings_Rotten + "\n" + "IMDB: " + ratings_IMDB +"\n" + poster
     chat_id =update.message.chat_id
-    bot.send_message(chat_id =chat_id, text =title)    
-
+    bot.send_message(chat_id =chat_id, text =output)
 
 
 def start(update, context):
     keyboard = [[InlineKeyboardButton("Movie Name", callback_data='1'),
-                 InlineKeyboardButton("IMDB Rating", callback_data='2')]]
+    			 InlineKeyboardButton("Series Name", callback_data='2'),
+                 InlineKeyboardButton("IMDB Rating", callback_data='3')]]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
@@ -26,9 +32,11 @@ def button(update, context):
     choice = context.callback_query.data
     query =context.callback_query
     if choice=='1':
-    	query.edit_message_text(text="Enter Movie Title")
+    	query.edit_message_text(text="Enter Movie Name")
+    elif choice=='2':
+    	query.edit_message_text(text="Enter Series Name")
     else:
-    	query.edit_message_text(text="Enter IMDB rating")
+    	query.edit_message_text(text="Enter IMDB Rating")
 
 
 def help(update, context):
@@ -36,10 +44,10 @@ def help(update, context):
 
 
 def main():
-    updater = Updater("TOKEN")
+    updater = Updater("707919520:AAE51Xcv4q3P0m7e83urLeTQrwwTG_iR8zo")
 
     updater.dispatcher.add_handler(CommandHandler('search', start))
-    updater.dispatcher.add_handler(MessageHandler(Filters.text, convert_uppercase))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, get_info))
     updater.dispatcher.add_handler(CallbackQueryHandler(button))
     updater.dispatcher.add_handler(CommandHandler('help', help))
 
