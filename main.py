5b5b30fcdd61fab7b3aa0dd68eaaf95e8b2dc60a
Amit Naik <mailamit70@gmail.com>
2019-04-25 17:56:54 -0400

    Removed IMDB search ratings

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageH
 import requests
 
 def get_info(update, context):
+    print("here")
     chat_id =context.message.chat_id
     text =context.message.text
     url ='http://www.omdbapi.com/?t='+text+'&apikey=9b24e033'
@@ -13,29 +14,14 @@ def get_info(update, context):
     genre =contents['Genre']
     awards =contents['Awards']
     poster =contents['Poster']
-    ratings_Rotten =contents['Ratings'][1]['Value']
     ratings_IMDB =contents['imdbRating']
-    output ="Name: " +title +"\n" + "Released in the year " +year +"\n" + "Actors: " + actors +"\n" + "Awards: " + awards +"\n" + "Genre: "+genre +"\n" + "Rotten Tomatoes: " +ratings_Rotten + "\n" + "IMDB: " + ratings_IMDB +"\n" + poster
-    
+    output ="Name: " +title +"\n" + "Released in the year " +year +"\n" + "Actors: " + actors +"\n" + "Awards: " + awards +"\n" + "Genre: "+genre +"\n" + "IMDB: " + ratings_IMDB +"\n" + poster
     update.send_message(chat_id =chat_id, text =output)
-
+    return
 
 def start(update, context):
-    keyboard = [[InlineKeyboardButton("Movie/Series Name", callback_data='1'),
-                 InlineKeyboardButton("IMDB Rating", callback_data='2')]]
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    context.message.reply_text('Please choose:', reply_markup=reply_markup)
-
-
-def button(update, context):
-    choice = context.callback_query.data
-    query =context.callback_query
-    print(query)
-    if choice=='1':
-    	query.edit_message_text(text="Enter Name")
-    else:
-    	query.edit_message_text(text="Enter IMDB Rating")
+    context.message.reply_text('Enter name to get details')
+    return 
 
 def help(update, context):
     context.message.reply_text("Use /search to start this bot.")
@@ -47,10 +33,9 @@ def unknown(update, context):
 
 
 def main():
-    updater = Updater("TOKEN")
+    updater = Updater("707919520:AAE51Xcv4q3P0m7e83urLeTQrwwTG_iR8zo")
 
     updater.dispatcher.add_handler(CommandHandler('search', start))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
     updater.dispatcher.add_handler(MessageHandler(Filters.text, get_info))
     updater.dispatcher.add_handler(CommandHandler('help', help))
     updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))
@@ -59,7 +44,6 @@ def main():
     updater.start_polling()
 
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
